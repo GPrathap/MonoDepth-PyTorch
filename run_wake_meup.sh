1d6e893f05f1b1b38ed96e615d757d7806be3c2f
GPrathap <ggeesara@gmail.com>
2018-12-26 06:31:58 +0000

    adding run script

---
+++ run_wake_meup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+python3 wakeme_up.py --data_dir /home/datasets/kitti/2011/2011_09_26/2011_09_26_drive_0001_sync/ --val_data_dir /home/datasets/kitti/2011/2011_09_26/2011_09_26_drive_0002_sync/ --model_path  /home/a.gabdullin/geesara/wakemeup/model --output_directory /home/a.gabdullin/geesara/wakemeup/result
